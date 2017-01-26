a70051002aa168f3dbdd2e375554cade07a6c17e
brentmc <brentmcivor.mail+workgithub@gmail.com>
2017-01-26 20:04:37 +1000

    Changed heading to Prod 3

--- modules/App.js
+++ modules/App.js
@@ -6,7 +6,7 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial (Staging 1)</h1>
+        <h1>React Router Tutorial (Prod 3)</h1>
         <ul role="nav">
         	<li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
           <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
